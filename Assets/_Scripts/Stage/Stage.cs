864dc2c0a923bd917e36f7fbf1e867d6226396ad
baekchul.jung <qorcjf7409@naver.com>
2025-08-15 18:47:36 +0900

    임시 리소스, 프리팹 추가

---
+++ Assets/_Scripts/Stage/Stage.cs
@@ -0,0 +1,36 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        PreLoad();
+        Setting();
+    }
+
+    private void PreLoad()
+    {
+    }
+
+   private void Setting()
+    {
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+    }
+}
