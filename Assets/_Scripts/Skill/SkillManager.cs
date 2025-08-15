41a868c4353833f1dba606ef9eeb0a1cdfe4138f
unengine <unengine@gmail.com>
2025-08-15 23:49:56 +0900

    스킬 더미 데이터 추가

--- Assets/_Scripts/Skill/SkillManager.cs
+++ Assets/_Scripts/Skill/SkillManager.cs
@@ -1,8 +1,19 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class SkillManager : MonoSingleton<SkillManager>
 {
-
+    public SkillDataScriptableObject skillData;
 
     private Dictionary<int, SkillBase> _skills = new();
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+
+    }
 }
