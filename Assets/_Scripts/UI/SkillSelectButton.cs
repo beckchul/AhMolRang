8d43abcfeb3b968e35fcb702dd864961c2249ea4
unengine <unengine@gmail.com>
2025-08-17 03:30:45 +0900

    ㅁㄴㅇ

--- Assets/_Scripts/UI/SkillSelectButton.cs
+++ Assets/_Scripts/UI/SkillSelectButton.cs
@@ -12,6 +12,24 @@ public class SkillSelectButton : MonoBehaviour
     [SerializeField]
     private TMP_Text buttonText;
 
+    [SerializeField]
+    private GameObject activeIconBox;
+
+    [SerializeField]
+    private GameObject pasiveIconBox;
+
+    [SerializeField]
+    private GameObject levelTextBox;
+
+    [SerializeField]
+    private GameObject strengthenTextBox;
+
+    [SerializeField]
+    private TMP_Text skillLabelText;
+
+    [SerializeField]
+    private TMP_Text levelText;
+
     public Action<SkillDataBase> OnClicked;
 
     private SkillDataBase skillInfo;
@@ -24,6 +42,30 @@ public class SkillSelectButton : MonoBehaviour
         skillInfo = SkillInfo;
         buttonIcon.sprite = skillInfo.skillIcon;
         buttonText.text = skillInfo.skillName;
+
+        if (skillInfo.skillType == SkillType.Active)
+        {
+            activeIconBox.SetActive(true);
+            pasiveIconBox.SetActive(false);
+        }
+        else
+        {
+            activeIconBox.SetActive(false);
+            pasiveIconBox.SetActive(true);
+        }
+
+        if (skillInfo.skillLevel >= 5)
+        {
+            levelTextBox.SetActive(false);
+            strengthenTextBox.SetActive(true);
+        }
+        else
+        {
+            levelTextBox.SetActive(true);
+            strengthenTextBox.SetActive(false);
+
+            levelText.text = "Lv." + skillInfo.skillLevel.ToString();
+        }
     }
 
     public void OnClickButton()
